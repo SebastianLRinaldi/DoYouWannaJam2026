65dca601a78112ba1516740ce1ac9f5ad3b67148
Sebastian Rinaldi <epicsauce2013@gmail.com>
2026-08-21 02:33:52 -0700

    bandaids

--- root.gd
+++ root.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 @onready var total_score: ScoreLabel = %TotalScore
 
+@onready var spawn_bandaid_button: Button = %SpawnBandaidButton
+const SPAWN_CURVE = preload("uid://co4qvjv0wuqx4")
+
 @onready var fl_score: ScoreLabel = %FLScore
 @onready var fl_score_2: ScoreLabel = %FLScore2
 @onready var fl_score_3: ScoreLabel = %FLScore3
@@ -20,42 +23,26 @@ var total_points = 100
 var points_removed
 
 
+var config = {
+	1: [0,0,0,0,0,0],
+	2: [3,3,3,3,3,3],
+	3: [1,2,3,4,5,6],
+}
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var x = 0
-	var y = 0
-	var x_offset = 26
-	var y_offset = 26
-	for temp_idx in range(electrode_count):
-		var ET = ELECTRODE.instantiate()
-		eletrode_container.add_child(ET)
-		ET.position = Vector2(26*x+x_offset, 26*y+y_offset)
-		x += 1
-		if x >= 4:
-			y += 1
-			x = 0
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
 
+func set_engery_by_level(level_key:int):
+	var energy_amounts = config[level_key]
+	for zone_idx in zones.size():
+		zones[zone_idx].calc_engery(energy_amounts[zone_idx])
+
+
 func _on_timer_timeout() -> void:
-	#zones[0].calc_engery(randi_range(0,3))
-	#zones[1].calc_engery(randi_range(0,3))
-	#zones[2].calc_engery(randi_range(0,3))
-	#zones[3].calc_engery(randi_range(0,3))
-	#zones[4].calc_engery(randi_range(0,3))
-	#zones[5].calc_engery(randi_range(0,3))
-	
-	zones[0].calc_engery(3)
-	zones[1].calc_engery(1)
-	zones[2].calc_engery(0)
-	zones[3].calc_engery(2)
-	zones[4].calc_engery(4)
-	zones[5].calc_engery(1)
+	set_engery_by_level(3)
 	
 	fl_score.update_score("B1", zones[0].energy)
 	fl_score_2.update_score("B2", zones[1].energy)
@@ -68,3 +55,40 @@ func _on_timer_timeout() -> void:
 	for zone in zones:
 		total += zone.energy
 	total_score.update_score("TS", total)
+
+var x = 0
+var y = 0
+var moving = false
+func _on_spawn_bandaid_button_pressed() -> void:
+	#var x_offset = 26
+	#var y_offset = 26
+	if not moving:
+		print(x)
+		moving = true
+		var ET = ELECTRODE.instantiate()
+		eletrode_container.add_child(ET)
+		#ET.position = Vector2(26*x+x_offset, 26*y+y_offset)
+
+		var tween = create_tween()
+		tween.set_parallel(true)
+		#audio_stream_player_2d.stream = move_sfx.pick_random()
+		#tween.tween_callback(audio_stream_player_2d.play)
+		var start = position
+		#var ran_x_range = randf_range(0, 130)
+		var end = start + Vector2(x*26, -SPAWN_CURVE.sample(x))
+		tween.tween_property(ET, "position", end, 0.5)
+		#tween.set_trans(Tween.TRANS_QUAD)
+		#tween.set_ease(Tween.EASE_OUT)
+		
+		tween.tween_property(ET,"scale",Vector2(0.5,1.5),0.05)
+		tween.tween_property(ET,"scale",Vector2(1.5,0.5),0.05)
+		tween.tween_property(ET,"scale",Vector2(1,1),0.05)
+		#tween.tween_interval(0.35) # Wait for half a second
+		await tween.finished
+		x += 1
+		if x >= 6:
+			x = 0
+		moving = false
+	
+	
+	
