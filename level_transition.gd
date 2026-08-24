09556dd97c60d3ea850ce546bead3f25cba41834
Sebastian Rinaldi <epicsauce2013@gmail.com>
2026-08-24 01:15:11 -0700

    main menu transitions

---
+++ level_transition.gd
@@ -0,0 +1,21 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
+var scene_to_load
+
+func change_scene_to(scene_path: String) -> void:
+	scene_to_load = scene_path
+	animation_player.play("close")
+	get_tree().paused = true
+	print("closed")
+	
+
+func _load_new_scene() -> void:
+	print("OPEN")
+	animation_player.play("open")
+	get_tree().call_deferred("change_scene_to_file", scene_to_load)
+	var tween = create_tween()
+	tween.tween_interval(2.0)
+	await tween.finished
+	get_tree().paused = false
+	
---
+++ main_menu/main_menu.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _on_start_pressed() -> void:
+	LevelTransition.change_scene_to("res://seb_test/level_1.tscn")
+
+func _on_tutorial_pressed() -> void:
+	pass # Replace with function body.
