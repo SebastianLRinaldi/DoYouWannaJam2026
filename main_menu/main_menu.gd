13c044fb4b0287ae7c1ea16110933e8c8b3c98a9
Sebastian Rinaldi <epicsauce2013@gmail.com>
2026-08-24 02:20:35 -0700

    pushing big for test

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@export_file("*.tscn") var first_level_path
+
 func _on_start_pressed() -> void:
-	LevelTransition.change_scene_to("res://test_level_A.tscn")
+	LevelTransition.change_scene_to(first_level_path)
 
 func _on_tutorial_pressed() -> void:
 	LevelTransition.change_scene_to("res://tutorial_1.tscn")
